20142fbd1ff4a2c25e351cbd5db4451e888bab57
Adam Brett Minkus <adam.minkus@gmail.com>
2019-02-19 01:11:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artistName: 'songTitles'};
 function updatePlaylist(object, name, song)
-function removeFromPlaylist(playlist,artistName){object[name]
+function removeFromPlaylist(playlist,artistName){object[name] = title; return playl
   delete playlist.Slowdive; return playlist
 }
