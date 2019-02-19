21f09bb6eed11d283c7e95c49f60613eca560cb7
Adam Brett Minkus <adam.minkus@gmail.com>
2019-02-19 01:05:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artistName: 'songTitles'};
-function updatePlaylist(playlist,artistName,songTitle)
+function updatePlaylist(object, name, song)
 {playlist.Slowdive ='Alison',playlist.My Bloody Valentine='Sometimes', playlist.Phil Ochs = 'Here\'s to the state of Mississippi';return playlist}
 function removeFromPlaylist(playlist,artistName){
   delete playlist.Slowdive; return playlist
