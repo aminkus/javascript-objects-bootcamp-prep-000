4fb800aa497561ff16d3deb7078ed4ac69909084
Adam Brett Minkus <adam.minkus@gmail.com>
2019-02-19 00:49:17 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,3 @@
+var playlist = {artistName: 'songTitles'};
+function updatePlaylist(playlist,artistName,songtTitle)
+{playlist.Slowdive ='Alison',playlist.My Bloody Valentine='Sometimes', playlist.Phil Ochs = 'Here\'s to the state of Mississippi';return playlist}
